009d4bbdeb40f8027c27dc946ef970ef0c8e304d
rakibman <rakibhossain455673@gmail.com>
2025-11-13 21:47:25 +0600

    theme togoling in log out page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,10 +14,10 @@ const Navbar = () => {
   const handelSignOut = () => {
     signOutUser()
       .then(() => {
-        toast.success('sign out successfull!')
+        toast.success("sign out successfull!");
       })
       .catch((err) => {
-        toast.error(err.code)
+        toast.error(err.code);
       });
   };
 
@@ -194,10 +194,10 @@ const Navbar = () => {
                     <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
                   </svg>
                   <input
-                  onChange={(e) => handleTheme(e.target.checked)}
+                    onChange={(e) => handleTheme(e.target.checked)}
                     type="checkbox"
                     defaultChecked={localStorage.getItem("theme") === "dark"}
-                  className="toggle mx-2"
+                    className="toggle mx-2"
                   />
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -217,10 +217,45 @@ const Navbar = () => {
             </div>
           </div>
         ) : (
-          <div>
+          <div className="flex gap-3 items-center">
+            <label className="flex cursor-pointer items-center my-5 gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              </svg>
+              <input
+                onChange={(e) => handleTheme(e.target.checked)}
+                type="checkbox"
+                defaultChecked={localStorage.getItem("theme") === "dark"}
+                className="toggle mx-2"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              </svg>
+            </label>
             <Link
               to={"/login"}
-              className="px-6 py-2 bg-white/10 backdrop-blur-md text-white border border-white/20 rounded-lg hover:bg-white/20 transition-all duration-300"
+              className="px-6 py-2 bg-white/10 backdrop-blur-md text-white border border-white/20 rounded-lg hover:bg-white/20 transition-all duration-300 h-fit"
             >
               Login
             </Link>
